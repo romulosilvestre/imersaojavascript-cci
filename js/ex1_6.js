dc885ed705ea6ce342cae1dbb601d728d863ee6b
dev <dev@grupocci.local>
2022-09-06 16:33:30 -0300

    pega ai Matheus

--- js/ex1_6.js
+++ js/ex1_6.js
@@ -1,13 +1,22 @@
-function calcularDistancia(){ 
-   //vem da API 
-   const gps={
-       disTotal:"1492.56",
-       disPercorrida:"1000",
-   } 
 
-   document.getElementById("disTotal").innerHTML = gps.disTotal
-   document.getElementById("disPercorrida").innerHTML = gps.disPercorrida
-   document.getElementById("disRestante").innerHTML = gps.disTotal-gps.disPercorrida
+function mostrarManutencao(){
 
+    const manutencao={    
+        aeronave:"Bandeirante",
+        observacao:"Esta em manutenção até segunda-feira",
+        valorDasPecas:1500,
+        valorDoServico:5000
+    }
+//Template String
+    document.getElementById("aeronave").innerHTML = `Aeronave: ${manutencao.aeronave}`
+    document.getElementById("observacao").innerHTML=`Observação:${manutencao.observacao}`
+    document.getElementById("valorDasPecas").innerHTML=`Valor das Peças: R$ ${manutencao.valorDasPecas}`
+    document.getElementById("valorDoServico").innerHTML=`Valor do Serviço: R$ ${manutencao.valorDoServico}`
+
+    /* + - * /    */
+    //PROCESSAMENTO
+    const total = manutencao.valorDasPecas+manutencao.valorDoServico
+    document.getElementById("valorTotal").innerHTML=`Total: R$ ${total}`    
 
 }
+
